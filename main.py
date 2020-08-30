b59491474f09a6764fb3fcdef52e6b7d12087a8f
Red <draco882003@gmail.com>
2020-08-31 03:24:49 +0530

    Add main and kv files and other resources

---
+++ main.py
@@ -0,0 +1,9 @@
+import kivy
+kivy.require('1.11.1')
+
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.properties import (NumericProperty, ObjectProperty,
+                             ReferenceListProperty)
+from kivy.uix.widget import Widget
+from kivy.vector import Vector
